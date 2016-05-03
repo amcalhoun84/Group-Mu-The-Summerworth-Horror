02af7745cdf81134f524009951644cf7eccfcdd7
Andrew Michael Calhoun <calhouna@onid.oregonstate.edu>
2016-05-03 12:26:32 -0700

    Graphics Engine
    Graphic Engines - Alpha 1.0

---
+++ UpdatedFiles-Andrew/graphics.hpp
@@ -0,0 +1,35 @@
+#ifndef _GRAPHICS_HPP
+#define _GRAPHICS_HPP
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <ncurses.h>
+#include <signal.h>
+
+#include "worldEngine.hpp"
+
+class Graphics 
+{
+
+private: 
+	// no need, we just transmit images.
+
+public:
+	Graphics();
+	~Graphics();
+
+	void splashScreen();
+	void introScreen();
+	void murderScene();
+	void deathScreen();
+	void insanityScreen();
+	void oneWhoSleeps();
+	void victoryScreen();
+	void creditsScreen();
+
+};
+
+#endif
